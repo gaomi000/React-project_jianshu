d87bbf8aa1bcd629ee917df3b0e7dd40c501409a
gaomi000 <gaomi000@163.com>
2020-04-20 22:18:15 +0800

    4.20学习

--- jianshu/src/pages/home/index.js
+++ jianshu/src/pages/home/index.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
 import Topic from './components/Topic';
 import List from './components/List';
 import Recommend from './components/Recommend';
 import Writer from './components/Writer';
+import { actionCreators } from './store';
 import { 
 	HomeWrapper,
 	HomeLeft,
 	HomeRight
 } from './style';
 
-class Home extends Component {
+class Home extends PureComponent {
+    handleScrollTop() {
+		window.scrollTo(0, 0);
+	}
     render(){
         return (
             <HomeWrapper>
@@ -25,6 +30,38 @@ class Home extends Component {
             </HomeWrapper>
         )
     }
+
+
+    componentDidMount() {
+        this.props.changeHomeData();
+        this.bindEvents();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.props.changeScrollTopShow);
+    }
+
+    bindEvents() {
+        window.addEventListener('scroll', this.props.changeScrollTopShow);
+    }
+
 }
 
-export default Home;
+const mapState = (state) => ({
+showScroll: state.getIn(['home', 'showScroll'])
+})
+
+const mapDispatch = (dispatch) => ({
+changeHomeData() {
+    dispatch(actionCreators.getHomeInfo());
+},
+changeScrollTopShow() {
+    if (document.documentElement.scrollTop > 100) {
+        dispatch(actionCreators.toggleTopShow(true))
+    }else {
+        dispatch(actionCreators.toggleTopShow(false))
+    }
+}
+});
+
+export default connect(mapState, mapDispatch)(Home);
